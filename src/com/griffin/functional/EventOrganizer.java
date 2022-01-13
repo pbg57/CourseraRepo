a1970696b6a9b712358c0a0d8a7e9a04b1e6f4c2
Phil Griffin <pbg57@yahoo.com>
2022-01-13 12:41:52 -0700

    Add comments and cleanup

--- src/com/griffin/functional/EventOrganizer.java
+++ src/com/griffin/functional/EventOrganizer.java
@@ -8,147 +8,46 @@ import java.util.List;
 //
 public class EventOrganizer {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
         EventDispatcherClient eventDispatcherClient = new EventOrganizer.EventDispatcherClient();
         eventDispatcherClient.runTests(new EventOrganizer.EventDispatcher());
 
     }
 
-    public void testEvent(Event event) {
-
-        event.onEventOccurance();
-
-    }
-
-    public void testEventProcess(EventProcess eventProcess) {
-
-        eventProcess.onEventOccuranceProcess(1);
-
-    }
-
-    public void testEventResult(EventResult eventResult) {
-
-        int result = eventResult.onEventOccuranceConfirm(1);
-
-        // printing the return value of
-
-        System.out.println("Print result value : " + result);
-
-    }
 
     // Annotation optional for SAMs
     @FunctionalInterface
     public interface Event {
 
-        public void onEventOccurance();
+        void onEventOccurrence();
 
     }
 
-
+    @FunctionalInterface
     public interface EventProcess {
 
-        public void onEventOccuranceProcess(int i);
+        void onEventOccurrenceProcess(int i);
 
     }
 
+    @FunctionalInterface
     public interface EventResult {
 
-        public int onEventOccuranceConfirm(int i);
+        int onEventOccurrenceConfirm(int i);
 
     }
 
-    public class EventOrganizerTest {
 
-        public void testEvent(Event event) {
-
-            event.onEventOccurance();
-
-        }
-
-        public void testEventProcess(EventProcess eventProcess) {
-
-            eventProcess.onEventOccuranceProcess(1);
-
-        }
-
-        public void testEventResult(EventResult eventResult) {
-
-            int result = eventResult.onEventOccuranceConfirm(1);
-
-            // printing the return value of
-
-            System.out.println("Print result value : " + result);
-
-        }
-
-    }
-
-    public class EventClient {
-
-        public static void main(String[] args) {
-
-            EventOrganizer eventTester = new EventOrganizer();
-
-            // calling no- argument functional interface
-
-            eventTester.testEvent(() -> System.out
-
-                    .println("event occurred : no argument syntax"));
-
-            // Note: example of replacing Lambda with method reference
-            eventTester.testEvent(System.out::flush);
-            eventTester.testEvent(System.out::flush);
-
-
-            // calling single- argument method of functional interface
-            // Note that any var name may be used (using 'j' instead of 'i')
-            eventTester.testEventProcess((j) -> System.out
-
-                    .println("event processed : one argument syntax : " + j));
-
-            // calling alternative single- argument method of functional interface. alternative
-
-            // syntax of omitting parenthesies
-
-            eventTester.testEventProcess(i -> System.out
-
-                    .println("event processed : one argument syntax : " + i));
-
-            // calling alternative single- argument method of functional interface. alternative
-
-            // syntax of wrapping expression body with curly braces
-
-            eventTester.testEventProcess((i) -> {
-
-                System.out.println("event processed : one argument syntax : " + i);
-
-            });
-
-            // calling single- argument method of functional interface which that returns
-
-            // the result
-
-            eventTester.testEventResult((i) -> {
-
-                i = i + 1;
-
-                return i;
-
-            });
-
-        }
-
-    }
     public static class EventDispatcher {
 
         // Store the dispatched events in the static class's List objects.
 
-        private List<Event> eventList = new ArrayList<>();
+        private final List<Event> eventList = new ArrayList<>();
 
-        private List<EventProcess> eventProcessList = new ArrayList<>();
+        private final List<EventProcess> eventProcessList = new ArrayList<>();
 
-        private List<EventResult> eventResultList = new ArrayList<>();
+        private final List<EventResult> eventResultList = new ArrayList<>();
 
         public void registerEventHandler(Event event) {
 
@@ -174,7 +73,7 @@ public class EventOrganizer {
 
             for (Event event : eventList) {
 
-                event.onEventOccurance();
+                event.onEventOccurrence();
 
             }
 
@@ -186,9 +85,9 @@ public class EventOrganizer {
 
             int i = 1;
 
-            for(EventProcess process : eventProcessList) {
+            for (EventProcess process : eventProcessList) {
 
-                process.onEventOccuranceProcess(i++);
+                process.onEventOccurrenceProcess(i++);
 
             }
 
@@ -200,11 +99,11 @@ public class EventOrganizer {
 
             final int i = 2;
 
-            for(EventResult process : eventResultList) {
+            for (EventResult process : eventResultList) {
 
-                int result = process.onEventOccuranceConfirm(i);
+                int result = process.onEventOccurrenceConfirm(i);
 
-                System.out.println("return result : "+ result);
+                System.out.println("return result : " + result);
 
             }
 
@@ -214,9 +113,8 @@ public class EventOrganizer {
 
     public static class EventDispatcherClient {
 
-        public void runTests (EventDispatcher eventDispatcher) {
+        public void runTests(EventDispatcher eventDispatcher) {
 
-        //    EventDispatcher eventDispatcher = new EventDispatcher();
 
             // Define no-argument functional interface
             // Event is a functional i/f whose SAM takes no args and returns void.
@@ -228,40 +126,53 @@ public class EventOrganizer {
             Event event2 = () -> System.out.println("event 2 occurred : no argument syntax");
 
 
-
             // Define single- argument method of functional interface
             // EventProcess is a functional i/f whose SAM takes one arg and returns void.
             // The EventProcess i/f instance process1 is implemented here by simply supplying a Lambda expression
             // taking one arg and returning void.
-            // Note that the single arg var name can be defined here in the impl (ie. can be i, j, jj, etc.)
+            // Note that the single arg var name can be defined here in the impl (i.e. can be i, j, jj, etc.)
 
-            EventProcess process1 = (jj) -> System.out.println("event processed : one argument syntax : "+ jj);
+            EventProcess process1 = (jj) -> System.out.println("event processed : one argument syntax : " + jj);
 
             // Use alternative syntax to define the Lambda. Alternative syntax is omitting parentheses.
 
-            EventProcess process2 = i -> System.out.println("event processed : one argument syntax : "+ i);
+            EventProcess process2 = i -> System.out.println("event processed : one argument syntax : " + i);
 
             // Use alternative syntax to define the Lambda. Alternative syntax is wrapping Lambda's expression
             // body with curly braces.
 
             EventProcess process3 = i -> {
 
-                System.out.println("event processed : one argument syntax : "+i);
+                System.out.println("event processed : one argument syntax : " + i);
 
             };
 
             // Define single- argument method of functional interface which also returns a non-void result
 
-            EventResult result1 = i -> { i = i + 2; return i; };
+            EventResult result1 = i -> {
+                i = i + 2;
+                return i;
+            };
 
-            EventResult result2 = i -> { i = i - 2; return i; };
+            EventResult result2 = i -> {
+                i = i - 2;
+                return i;
+            };
 
-            EventResult result3 = i -> { i = i * 2; return i; };
+            EventResult result3 = i -> {
+                i = i * 2;
+                return i;
+            };
 
-            EventResult result4 = i -> { i = i / 2; return i; };
+            EventResult result4 = i -> {
+                i = i / 2;
+                return i;
+            };
 
-            EventResult result5 = i -> { i = i % 2; return i; };
-
+            EventResult result5 = i -> {
+                i = i % 2;
+                return i;
+            };
 
 
             // registering events. The EventDispatcher stores the Event events.
@@ -273,7 +184,7 @@ public class EventOrganizer {
             // dispatchEvent() iterates over the stored events and calls its SAM method.
             eventDispatcher.dispatchEvent();
 
-            System.out.println("");
+            System.out.println();
 
             // registering event processes.  The EventDispatcher stores the EventProcess events.
 
@@ -287,8 +198,7 @@ public class EventOrganizer {
 
             eventDispatcher.processEvents();
 
-            System.out.println("");
-
+            System.out.println();
 
 
             // registering result- based events. The EventDispatcher stores the EventResult events.
--- src/com/griffin/functional/LambdaExamples1.java
+++ src/com/griffin/functional/LambdaExamples1.java
@@ -3,16 +3,20 @@ package com.griffin.functional;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.IntFunction;
 
 public class LambdaExamples1 {
 
 
- //   Any interface with a SAM(Single Abstract Method) is a functional interface, and its implementation may
-    //   be treated as lambda expressions.
+//
+// This code was copied from a course example and modified here.
+//
 
-//    Note that Java 8's default methods are not abstract and do not count; a functional interface may still
-//    have multiple default methods. We can observe this by looking at the Function's documentation.
+    /*
+       Any interface with a SAM(Single Abstract Method) is a functional interface, and its implementation may
+       be treated as lambda expressions.
+       Note that Java 8's default methods are not abstract and do not count; a functional interface may still
+       have multiple default methods. We can observe this by looking at the Function's documentation.
+    */
 
     // Note that this annotation is optional, because the interface declaration is a SAM.
     @FunctionalInterface
@@ -22,20 +26,20 @@ public class LambdaExamples1 {
     }
 
     public byte[] transformArray(short[] array, ShortToByteFunction function) {
+        // New byte array sized for input short array
         byte[] transformedArray = new byte[array.length];
+
+        // Iterate, calling functional interface to do the short to byte conversion
         for (int i = 0; i < array.length; i++) {
             transformedArray[i] = function.applyAsByte(array[i]);
         }
         return transformedArray;
     }
 
-    public static void main(String [] args) {
-       LambdaExamples1 lam =  new LambdaExamples1();
-       lam.runTest1();
-       lam.runTest2();
-
-
-
+    public static void main(String[] args) {
+        LambdaExamples1 lam = new LambdaExamples1();
+        lam.runTest1();
+        lam.runTest2();
     }
 
 
@@ -47,7 +51,6 @@ public class LambdaExamples1 {
         // The method is the single abstract method (SAM) of the functional interface, applyAsByte, in this case.
         // The SAM defines the type (short, in this case), being operated on. Note that
         // The passed Lambda does not have to use 's' and can use any name.
-       // byte[] transformedArray = transformArray(array, s -> (byte) (s * 2));
         byte[] transformedArray = transformArray(array, s -> (byte) (s * 2));
 
 
@@ -56,23 +59,35 @@ public class LambdaExamples1 {
         System.out.println(Arrays.toString(array));
 
         System.out.println(Arrays.toString(transformedArray));
-        //   assertArrayEquals(expectedArray, transformedArray);
 
-//        short shortReturn = new IntFunction<>().apply(5);
-//        System.out.println("short output:"+shortReturn);
+        if (Arrays.equals(expectedArray, transformedArray))
+            System.out.println("Arrays match: success");
+        else
+            System.out.println("Arrays do not match: error");
 
     }
 
     public void runTest2() {
         Map<String, Integer> nameMap = new HashMap<>();
 
-        // computeIfAbsent is a Map method which accepts a Key and mapping Function (Lambda or method reference}
-        Integer value = nameMap.computeIfAbsent("John", s -> s.length());
+        // computeIfAbsent is a Map method which accepts a Key and mapping Function (Lambda or method reference)
+        Integer value = nameMap.computeIfAbsent("John", s -> s.length());   // example, only
         System.out.println("Key: John value: " + nameMap.get("John"));
+
+        if (nameMap.get("John") == "John".length())
+            System.out.println("Map values match: success");
+        else
+            System.out.println("Map values do not match: error");
 
         // Note replacement of Lambda with method reference
         nameMap.computeIfAbsent("Dylan", String::length);
         System.out.println("Key: Dylan value: " + nameMap.get("Dylan"));
+
+        if (nameMap.get("Dylan") == "Dylan".length())
+            System.out.println("Map values match: success");
+        else
+            System.out.println("Map values do not match: error");
+
 
 
     }
