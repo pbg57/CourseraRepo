d0ddfe622f7722f36d84c5c1fcd0981e38f5e327
Phil Griffin <pbg57@yahoo.com>
2022-01-17 09:57:55 -0700

    cleanup

--- src/com/griffin/generics/samples/LeetCode.java
+++ src/com/griffin/generics/samples/LeetCode.java
@@ -1,6 +1,5 @@
 package com.griffin.generics.samples;
 
-import java.sql.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -15,18 +14,24 @@ public class LeetCode {
     void testRunner() {
         // Run tests
         int[] intArray = {2, 4, 5, 6, 3, 55, 4, 66};
+        /*
+        Return the two indices of the addends for the input target sum.
+         */
         int[] retValue = new Solution().twoSum(intArray, 61);
         System.out.println("twoSum input: " + Arrays.toString(intArray));
         System.out.println("twoSum output: " + Arrays.toString(retValue));
 
-        int[] intArray2 = {-1, 0, 1, 2, -1, -4};
+        /*
+        Find the three array entries whose sum equals zero, and whose values form a unique set of addends.
+         */
+//        int[] intArray2 = {-1, 0, 1, 2, -1, -4};
 //        int[] intArray2 = {1, 2, -2, -1};
-//        int[] intArray2 = {-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0};
+        int[] intArray2 = {-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0};
 
         System.out.println("3Sum input: " + Arrays.toString(intArray2));
         List<List<Integer>> retValue2 = new Solution3Sum().threeSum(intArray2);
 
-        for (List list : retValue2) {
+        for (List<Integer> list : retValue2) {
             System.out.println("3Sum output: " + list.toString());
         }
 
@@ -34,9 +39,14 @@ public class LeetCode {
 //        String b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
         String a = "11";
         String b = "101";
-        String result = new SolutionAddBinary().addBinary(a, b);
+        /*
+        Add two binary numbers and return them as a string.
+         */
+        new SolutionAddBinary().addBinary(a, b);
 
-
+        /*
+        Given an integer array with duplicate entries, find the single unique entry
+         */
         int[] inArray = {5, 3, 2, 2, 1, 1, 3};
         int resultInt = new SolutionSingleNumber().singleNumber(inArray);
         System.out.println("SingleNUmber found " + resultInt);
@@ -46,31 +56,34 @@ public class LeetCode {
         int[] nums = {1, 2, 3};
         List<Integer> inpList = Arrays.stream(nums).boxed().collect(Collectors.toList());
         System.out.println("int array to Integer List:");
-        inpList.stream().forEach(System.out::println);
+        inpList.forEach(System.out::println);
 
         // How to use streams to convert a object String []  into a List
         String[] strings = {"one", "two", "three"};
         List<String> strList = Arrays.stream(strings).toList();
         System.out.println("string array to List");
-        strList.stream().forEach(System.out::println);
+        strList.forEach(System.out::println);
 
         int[] inArra2y = {5, 3, 2, 2, 3, 2, 3};
         int resultInt2 = new SolutionSingleNumber2().singleNumber(inArra2y);
         System.out.println("SingleNUmber2 found " + resultInt2);
 
-        String inputString = new String("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT");
+        String inputString = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
         List<String> resultList = new SolutionFindRepeatedSequence().findRepeatedDnaSequences(inputString);
         System.out.println("Find Repeated Sequence: ");
-        resultList.stream().forEach(System.out::println);
+        resultList.forEach(System.out::println);
     }
 
 
-    class Solution {
+    static class Solution {
+        /*
+        Find the two indices whose sum equals the target value.
+         */
         public int[] twoSum(int[] nums, int target) {
             int nextIndex = 0;
+
+            // Nested loops through the array to find the indices
             for (int j = nextIndex; j < nums.length - 1; j++) {
-
-
                 for (int i = j + 1; i < nums.length; i++) {
                     if ((nums[nextIndex] + nums[i]) == target) {
                         int[] retArray = new int[2];
@@ -85,13 +98,21 @@ public class LeetCode {
         }
     } // End class Solution
 
-    class Solution3Sum {
+    static class Solution3Sum {
+        /*
+        Find the three array entries whose sum equals zero, and whose values form a unique set of addends.
+         */
         public List<List<Integer>> threeSum(int[] nums) {
 
-            // start with i=0; j=i+1; k=i+2 and loop until k>nums.length
-            List<List<Integer>> intList = new ArrayList<List<Integer>>();
+            // Return value is List of integer lists for each unique set of integers totaling zero.
+            List<List<Integer>> intList = new ArrayList<>();
+
+            // Use intSet to represent unique HashSet triplets that have been found.
             List<HashSet<Integer>> intSet = new ArrayList<>();
 
+            /*
+            Start with i=0; j=i+1; k=i+2 and loop until k>nums.length
+             */
             for (int i = 0; i < nums.length; i++) {
                 for (int j = i + 1; j < nums.length; j++) {
                     for (int k = i + 2; k < nums.length; k++) {
@@ -104,6 +125,7 @@ public class LeetCode {
                             solnArray[2] = nums[k];
                             List<Integer> itListInt = Arrays.asList(solnArray);
                             if (intList.size() == 0) {
+                                // First entry into return List
                                 intList.add(itListInt);
                                 HashSet<Integer> hashInt = new HashSet<>();
                                 hashInt.add(nums[i]);
@@ -115,7 +137,7 @@ public class LeetCode {
                                 // Create a List of HashSets of Integers to represent unique triplets
                                 // Determine if we have added this triplet already?;
                                 boolean found = false;
-                                for (HashSet set : intSet) {
+                                for (HashSet<Integer> set : intSet) {
                                     int count = 0;
                                     for (Integer itInt : itListInt) {
                                         if (set.contains(itInt))
@@ -128,6 +150,7 @@ public class LeetCode {
                                     }
                                 }
                                 if (!found) {
+                                    // Have not seen this unique set of addends. Add to return
                                     System.out.println("New Triplet found");
                                     intList.add(itListInt);
                                     HashSet<Integer> hashInt = new HashSet<>();
@@ -145,17 +168,16 @@ public class LeetCode {
         }
     }
 
-    class SolutionAddBinary {
+    static class SolutionAddBinary {
         public String addBinary(String a, String b) {
-            // take two binary numbers and add them. return as binary string
+            // take two binary numbers and add them. Return as binary string
 
             // Binary to base 10
-//            Long aLong = Long.parseLong(a, 2);
             Integer aInt = Integer.parseInt(a, 2);
 
             System.out.println("addBinary input " + a + " " + b);
             Integer bInt = Integer.parseInt(b, 2);
-            Integer intSum = aInt + bInt;
+            int intSum = aInt + bInt;
 
             String binResult = Integer.toBinaryString(intSum);
             System.out.println("addBinary result " + binResult);
@@ -164,14 +186,17 @@ public class LeetCode {
         }
     }
 
-    class SolutionSingleNumber {
+    static class SolutionSingleNumber {
+        /*
+        Given an integer array with duplicate entries, find the single unique entry
+         */
         public int singleNumber(int[] nums) {
             Set<Integer> inpSet = new HashSet<>();
             List<Integer> inpList = Arrays.stream(nums).boxed().collect(Collectors.toList());
 
             for (Integer inta : inpList) {
                 if (!inpSet.add(inta)) {
-                    // Already exists in Set, so remove
+                    // Already exists in Set, so remove because this is a duplicate in the input array
                     inpSet.remove(inta);
                 }
             }
@@ -184,8 +209,7 @@ public class LeetCode {
         }
     }
 
-    class SolutionSingleNumber2
- {
+    static class SolutionSingleNumber2 {
         public int singleNumber(int[] nums) {
 
             int retInt = 0;
@@ -213,11 +237,12 @@ public class LeetCode {
                     break;
                 }
             }
+
             return retInt;
         }
     }
 
-    class SolutionFindRepeatedSequence {
+    static class SolutionFindRepeatedSequence {
         public List<String> findRepeatedDnaSequences(String s) {
             // Process String, collecting 10 letter long strings based on overall length.
             // For each, construct substring and use String.indexOf() to locate unique instances of the substring.
