d4c7d287215849b218058488c88289609749a257
Phil Griffin <pbg57@yahoo.com>
2022-01-20 12:34:53 -0700

    cleanup Folders
    MathUtils, rm RoutePlanner
    
    Add Song, rem TestDome, cleanup

--- src/com/griffin/testdome/Folders.java
+++ src/com/griffin/testdome/Folders.java
@@ -3,6 +3,7 @@ package com.griffin.testdome;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -13,41 +14,44 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
+/*
+Write a class that processes an XML document or string and searches for all folder elements with
+attributes beginning with a given string or character.
+ */
 public class Folders {
-    public static Collection<String> folderNames(String xml, char startingLetter) throws Exception {
-        DocumentBuilderFactory dbf =  DocumentBuilderFactory.newInstance();
+    public static Collection<String> folderNames(String xml, char startingLetter)  {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         Collection<String> retColl = new ArrayList<>();
 
         try {
-                DocumentBuilder db = dbf.newDocumentBuilder();
-                Document doc = db.parse(new InputSource(new StringReader(xml)));
-                doc.getDocumentElement().normalize();
+            // Use w3c lib to parse the XML into a document.
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(new InputSource(new StringReader(xml)));
+            doc.getDocumentElement().normalize();   // Puts all text nodes under this node
 
             System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
             Node firstNode = doc.getDocumentElement().getAttributeNode("name");
             System.out.println(" Element Attr :" + firstNode);
 
-                NodeList nodeList = doc.getElementsByTagName("folder");
+            NodeList nodeList = doc.getElementsByTagName("folder");
             System.out.println("Node List Size : " + nodeList.getLength());
 
-                for (int idx = 0; idx < nodeList.getLength(); idx++) {
-                    Node node = nodeList.item(idx);
-                    System.out.println("NOde Name : " + node.getNodeName());
+            for (int idx = 0; idx < nodeList.getLength(); idx++) {
+                Node node = nodeList.item(idx);
+                System.out.println("Node Name : " + node.getNodeName());
 
-                    if (node.getNodeName().equals("folder")) {
-                        System.out.println("Folder Element : " + node.getNodeName());
-                        Element element = (Element)node;
-                        String attrName = element.getAttribute("name");
-                        System.out.println("Folder Attr : " + attrName);
-                        if (attrName.startsWith(String.valueOf(startingLetter))) {
-                            retColl.add(attrName);
-                        }
+                if (node.getNodeName().equals("folder")) {
+                    System.out.println("\tFolder Element : " + node.getNodeName());
+                    Element element = (Element) node;
+                    String attrName = element.getAttribute("name");
+                    System.out.println("\tFolder Attr : " + attrName);
+                    if (attrName.startsWith(String.valueOf(startingLetter))) {
+                        retColl.add(attrName);
                     }
                 }
+            }
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
         }
@@ -59,14 +63,16 @@ public class Folders {
         String xml =
                 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
                         "<folder name=\"c\">" +
-                        "<folder name=\"program files\">" +
-                        "<folder name=\"uninstall information\" />" +
-                        "</folder>" +
-                        "<folder name=\"users\" />" +
+                            "<folder name=\"program files\">" +
+                             "<folder name=\"uninstall information\" />" +
+                            "</folder>" +
+                            "<folder name=\"users\" />" +
                         "</folder>";
 
-        Collection<String> names = folderNames(xml, 'u');
-        for(String name: names)
-            System.out.println(name);
+        char startingLetter = 'u';
+        Collection<String> names = folderNames(xml, startingLetter);
+        System.out.println("Found folder element attributes beginning with '"+startingLetter+"'");
+        for (String name : names)
+            System.out.println("\t"+name);
     }
 }
--- src/com/griffin/testdome/MathUtils.java
+++ src/com/griffin/testdome/MathUtils.java
@@ -1,11 +1,19 @@
 package com.griffin.testdome;
 
+/*
+Utility math functions.
+ */
 public class MathUtils {
+
+    /*
+    Returns the double precision sum of two ints.
+     */
     public static double average(int a, int b) {
         return ((double) a + (double) b) / 2;
     }
 
     public static void main(String[] args) {
-        System.out.println(average(2,1));
+
+        System.out.println("double val: " + average(2,1));
     }
 }
