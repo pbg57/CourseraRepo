96f7539325a3518f5eb6787d9c66abdfcfff0278
Phil Griffin <pbg57@yahoo.com>
2022-01-20 10:08:45 -0700

    cleanup

--- src/com/griffin/testdome/AlertService.java
+++ src/com/griffin/testdome/AlertService.java
@@ -1,16 +1,19 @@
 package com.griffin.testdome;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
+/*
+Design an interface for an alert storage system allowing creation and retrieval of date-based alerts.
+Provide an implementation of the alert interface.
+ */
 class AlertService {
 
-    private  AlertDAO storage = null;
+    private final AlertDAO storage;
 
-
-    public  AlertService (AlertDAO alertDAO) {
+    /*
+    Construct an instance of the AlertService based on the AlertDAO interface:
+     */
+    public AlertService(AlertDAO alertDAO) {
         this.storage = alertDAO;
     }
 
@@ -21,17 +24,44 @@ class AlertService {
     public Date getAlertTime(UUID id) {
         return this.storage.getAlert(id);
     }
+
+    public static void main(String [] args) {
+        AlertService alertService = new AlertService(new MapAlertDAO());
+        alertService.testRunner();
+    }
+
+    public void testRunner() {
+        List<UUID> uuidList = new ArrayList<>();
+
+        // Create/retrieve sample alerts
+        uuidList.add(raiseAlert());
+        uuidList.add(raiseAlert());
+        uuidList.add(raiseAlert());
+        uuidList.add(raiseAlert());
+        uuidList.add(raiseAlert());
+
+        // Retrieve each UUID and check that AlertService recognizes it
+        for (UUID uuid: uuidList) {
+            if (getAlertTime(uuid) == null) {
+                System.out.println("AlertService error cannot find UUID : " + uuid);
+            }
+        }
+    }
 }
 
+/*
+The simple alert data-access-object interface, which provides alert persistence.
+ */
 interface AlertDAO {
-
-    public UUID addAlert(Date time);
-
-    public Date getAlert(UUID id);
+    UUID addAlert(Date time);
+    Date getAlert(UUID id);
 }
 
+/*
+Interface implementation.
+ */
 class MapAlertDAO implements AlertDAO {
-    private final Map<UUID, Date> alerts = new HashMap<UUID, Date>();
+    private final Map<UUID, Date> alerts = new HashMap<>();
 
     public UUID addAlert(Date time) {
         UUID id = UUID.randomUUID();
@@ -40,6 +70,6 @@ class MapAlertDAO implements AlertDAO {
     }
 
     public Date getAlert(UUID id) {
-        return this.alerts.get(id);
+        return this.alerts.get(id);     // No mapping for id will return null
     }
 }
